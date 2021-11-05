443bd9d6709f5bf61483d34deee7c1548b7bb571
Zachary Zawaideh <zachzawaideh@yahoo.com>
2021-11-04 20:10:56 -0500

    second commit

--- database.js
+++ database.js
@@ -9,12 +9,12 @@ require("dotenv").config();
 //    port: process.env.PG_PORT
 //};
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL
+const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
-    connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig
+    connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 2001;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client/build")))
+//app.use(express.static(path.join(__dirname, "client/build")))
 //npm run build
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build")))
